a72ddf8d80fab9fd152039078e33b65d5d8628cc
hlai16 <gloriajoanlai@gmail.com>
2021-09-20 18:03:39 -0400

    headTest.js with M and C

--- head.js
+++ head.js
@@ -7,6 +7,4 @@ const head = function(array) {
   return array[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
+module.exports = head;
