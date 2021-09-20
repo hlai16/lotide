6c394da6c7170b28568e3f72ed4b61c21192563e
hlai16 <gloriajoanlai@gmail.com>
2021-09-20 16:08:13 -0400

    middle.js modules set

--- middle.js
+++ middle.js
@@ -1,21 +1,17 @@
 const middle = function(array) {
   const length = array.length;
   if (length <= 2) {
-    console.log('[]');
+    return [];
   } else {
     const half = Math.floor(length / 2);
     if (length % 2 === 1) {
-      console.log(array.slice(half, (length - half)));
+      return array.slice(half, (length - half));
     } else if (length % 2 === 0) {
-      console.log(array.slice((half - 1), (length - half + 1)));
+      return array.slice((half - 1), (length - half + 1));
     }
   }
   
 };
 
-middle([1]); // => []
-middle([1, 2]); // => []
-middle([1, 2, 3]); // => [2]
-middle([1, 2, 3, 4, 5]); // => [3]
-middle([1, 2, 3, 4]); // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+module.exports = middle;
+
